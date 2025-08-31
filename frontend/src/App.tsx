ba604ce18a2c6b8734024ebc569ec442ded48638
Jodie Glantzlen <jodie.glantzlen@gmail.com>
2025-08-31 16:29:37 +0200

    Add simple hello fetch to check FE-BE connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 import { Button } from '@mui/material'
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  const handleClick = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/hello");
+      const data = await res.json();
+      setMsg(data.message);
+
+    } catch (e) {
+      console.error("Error fetching from backend:", e);
+      setMsg("Something went wrong!");
+    }
+  };
+  
   return (
-    <Button variant='contained'>Hallo MUI button!</Button>
+    <div>
+      <Button variant='contained' onClick={handleClick}>Hallo MUI button!</Button>
+      <div>{msg}</div>
+    </div>
   )
 }
 
