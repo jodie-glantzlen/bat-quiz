a5c6c8fda48f44188dc418f43a1632a0d88c9267
Jodie Glantzlen <jodie.glantzlen@gmail.com>
2025-09-02 12:03:58 +0200

    Display current question based on index on Continue

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import questions from '../data/questions.json'
 import QuestionCard from './QuestionCard'
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Game() {
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+
+  const currentQuestion = questions[currentQuestionIndex]
+
+  const navigate = useNavigate()
+
+  const handleContinue = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((prev) => prev + 1)
+    } else {
+      navigate("/finish")
+    }
+  }
+
   return (
     <div className='game-container'>
-    <h1>I'm the Game screen!</h1>
-    {
-      questions.map((q) => (
-        <QuestionCard question={q}/>
-      ))
-    }
+      <h1>Question {currentQuestionIndex + 1}/{questions.length}</h1>
+      <QuestionCard question={currentQuestion}/>
+      <Button onClick={handleContinue}>Continue</Button>
     </div>
   )
 }
--- frontend/src/components/QuestionCard.tsx
+++ frontend/src/components/QuestionCard.tsx
@@ -3,7 +3,6 @@ import type { QuestionCardProps } from "../../types"
 function QuestionCard({question}: QuestionCardProps) {
   return (
     <div>
-      <h1>I'm the question card!</h1>
       <img src={question.image} alt="very cute bat"/>
       {
         question.options.map((o) => (
