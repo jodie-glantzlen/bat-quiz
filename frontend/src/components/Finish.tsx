49ec2832735035713edc5c8cfe65e7c9d6662fbe
Jodie Glantzlen <jodie.glantzlen@gmail.com>
2025-09-04 08:01:18 +0200

    Style <Finish />

--- frontend/src/components/Finish.tsx
+++ frontend/src/components/Finish.tsx
@@ -1,38 +1,44 @@
 import { useEffect, useState } from "react";
-import type {Player, PlayerProps} from "../../../types"
+import type { Player, PlayerProps } from "../../../types";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { getScores } from "../api/api";
+import "../styles/Finish.css";
 
 function Finish({ player, setPlayer }: PlayerProps) {
 
-  const [leaderboard, setLeaderboard] = useState<Player[]>([])
+  const [leaderboard, setLeaderboard] = useState<Player[]>([]);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    getScores().then(setLeaderboard)
-  }, [])
+    getScores().then(setLeaderboard);
+  }, []);
 
-  const handleClick = () => {
-    setPlayer({ name: "", score: 0 })
-    navigate("/")
-  }
+  const handleClick = (): void => {
+    setPlayer({ name: "", score: 0 });
+    navigate("/");
+  };
 
   return (
-    <>
+    <div className="finish-container">
       <h1>Well played, {player.name}!</h1>
-      <h1>Your score: {player.score}</h1>
-      {
-        leaderboard.map(player => (
-          <ol>
-            <li key={player.name}>{player.name}: {player.score}</li>
-          </ol>
-        ))
-      }
+      <h3>Your score: {player.score}</h3>
+      <div className="leaderboard">
+        <ol>
+          {
+            leaderboard.map(player => (
+              <li key={player.name}>
+                <span className="name">{player.name}</span>
+                <span className="score">{player.score}</span>
+              </li>
+            ))
+          }
+        </ol>
+      </div>
       <Button onClick={handleClick}>New Game</Button>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Finish
+export default Finish;
